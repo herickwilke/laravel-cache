be920fb721ac89c660b06de666f9bad356b6d18c
herick <herickcwilke@gmail.com>
2020-01-09 09:37:37 -0300

    Testes com redis, fim da section

--- app/Http/Controllers/ProdutosControlador.php
+++ app/Http/Controllers/ProdutosControlador.php
@@ -4,11 +4,18 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Produto;
+use Illuminate\Support\Facades\Cache;
 
 class ProdutosControlador extends Controller
 {
     function index() {
-        $produtos = Produto::with('categorias')->get();
+        $expiracao = 1; //minutos
+
+        $produtos = Cache::remember('todososprodutos', $expiracao, function(){
+            info('Passei por aqui');
+            return Produto::with('categorias')->get();
+        });
+
         return view('produtos', compact(['produtos']));
     }
 }
